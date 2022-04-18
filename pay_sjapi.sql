0e290f2c4547323b864db8207f8e0f7a8c0870f3
OuLu001 <103594295+OuLu001@users.noreply.github.com>
2022-04-18 08:54:44 +0800

    Add files via upload

---
+++ pay_sjapi.sql
@@ -0,0 +1,4 @@
+INSERT INTO `pay_sjapi` (`id`, `apiname`, `myname`, `shid`, `key`, `zhanghu`, `edit_date`, `payname`, `fl`, `xz`, `tzurl`) VALUES
+(5, 'yibao', '易宝', '10011852577', '550hc5lwn93toxocdk8348bc36y18zyqit0voqo6v4kuahaly3pzxczd3o5m', 'lt444com@126.com', '2014-11-28', 'Yibao', '0.300', 1, NULL),
+(6, 'huanxunips', '环迅IPS', '', '', '', '2015-04-03', 'Huanxunips', '0.000', 0, '');
+
